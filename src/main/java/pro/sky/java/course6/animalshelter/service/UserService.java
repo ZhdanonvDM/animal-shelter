bccedce85d501e4eaad227878bdada8a331f7abb
ZhdanovDM <gdanovdm@gmail.com>
2023-10-10 20:07:11 +0500

    Animal-shelter

---
+++ src/main/java/pro/sky/java/course6/animalshelter/service/UserService.java
@@ -0,0 +1,20 @@
+package pro.sky.java.course6.animalshelter.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.java.course6.animalshelter.entity.User;
+import pro.sky.java.course6.animalshelter.repository.UserRepository;
+
+@Service
+public class UserService {
+
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser (User user) {
+        return userRepository.saveAndFlush(user);
+    }
+}
